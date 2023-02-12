180c4c67fed160745a605ba73fd567550a757781
snieda26 <snieda.programmer@gmail.com>
2023-02-12 21:57:05 +0200

    init project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Button />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import http from 'http'
+import cors from 'cors'
+import { router } from './route.js';
+import { Server } from 'socket.io';
+
+
+const PORT = process.env.PORT || 3005
+const app = express()
+
+app.use(cors({ origin: "*" }))
+app.use(router)
+
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: "*",
+    methods: ["GET"]
+})
+
+server.listen(PORT, () => {
+    console.log("Server started on " + PORT)
+})
