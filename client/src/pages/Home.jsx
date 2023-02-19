f6c001bd05276a89ff599683a4792f3dc9637058
snieda26 <snieda.programmer@gmail.com>
2023-02-19 23:05:25 +0200

    added login for room

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,39 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 import styles from '../styles/Home.module.css'
 
 
 function Home() {
+    const [values, setValues] = useState({ name: '', room: '' })
+    const handleChange = e => setValues(prev => ({ ...prev, [e.target.name]: e.target.value }))
     return (
         <div className={styles.wrap}>
-            <div className={styles.container}></div>
-            <h1 className={styles.heading}>Join</h1>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>Join</h1>
 
+                <form className={styles.form}>
+                    <div className={styles.group}>
+                        <input type="text"
+                            name="name"
+                            placeholder="Username"
+                            className={styles.input}
+                            onChange={handleChange}
+                            value={values.name}
+                            autoComplete="off" />
+                    </div>
 
-            <form className={styles.form}>
-                <div className={styles.group}>
-                    <input type="text"
-                        name="username"
-                        placeholder="Username"
-                        className={styles.input}
-                        onChange={() => { }}
-                        autoComplete="off" />
-                </div>
+                    <div className={styles.group}>
+                        <input type="text"
+                            name="room"
+                            placeholder="room"
+                            className={styles.input}
+                            onChange={handleChange}
+                            value={values.room}
+                            autoComplete="off" />
+                    </div>
 
-                <div className={styles.group}>
-                    <input type="text"
-                        name="room"
-                        placeholder="room"
-                        className={styles.input}
-                        onChange={() => { }}
-                        autoComplete="off" />
-                </div>
+                    <Link to="/chat">
+                        <button className={styles.button}>Sign In</button>
+                    </Link>
+                </form>
+            </div>
 
-                <Link to="/chat">
-                    <button className={styles.button}>Sign In</button>
-                </Link>
-            </form>
         </div>
     )
 }
